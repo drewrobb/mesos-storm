245b4be0813397429a0cdc552adae66057b87263
darinj <darinj.work@gmail.com>
2016-01-10 12:43:01 -0500

    Adds support for storm-0.10.0 - Created Multi-Project Build and v9-v10 shim. - Set up versioning as discussed in issue #77 - Updated Readme.MD - Updated .gitignore for _docker, *.zip, *.tgz - Updated vagrant scripts
    Adds support for mesos-0.26.0
    - use reflection to choose between setSecretBytes or setSecret

--- vagrant/start-nimbus.sh
+++ vagrant/start-nimbus.sh
@@ -6,13 +6,10 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 RELEASE=`grep -1 -A 0 -B 0 '<version>' /vagrant/pom.xml | head -n 1 | awk '{print $1}' | sed -e 's/.*<version>//' | sed -e 's/<\/version>.*//'`
-STORM_RELEASE=${STORM_RELEASE:-`grep -1 -A 0 -B 0 '<storm.default.version>' /vagrant/pom.xml | head -n 1 | awk '{print $1}' | sed -e 's/.*<storm.default.version>//' | sed -e 's/<\/storm.default.version>.*//'`}
-MESOS_RELEASE=${MESOS_RELEASE:-`grep -1 -A 0 -B 0 '<mesos.default.version>' /vagrant/pom.xml | head -n 1 | awk '{print $1}' | sed -e 's/.*<mesos.default.version>//' | sed -e 's/<\/mesos.default.version>.*//'`}
-CLASSIFIER=storm${STORM_RELEASE}-mesos${MESOS_RELEASE}
 
 echo "`date` - Starting nimbus!!" > start-nimbus.log
 
-cd /vagrant/_release/storm-mesos-${RELEASE}-${CLASSIFIER}
+cd /vagrant/_release/storm-mesos-${RELEASE}-*
 
 # kill existing MesosNimbus and storm UI processes
 kill `ps aux | grep MesosNimbu[s] | awk '{print $2}'` &> /dev/null || /bin/true
--- vagrant/submit-topology.sh
+++ vagrant/submit-topology.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 RELEASE=`grep -1 -A 0 -B 0 '<version>' /vagrant/pom.xml | head -n 1 | awk '{print $1}' | sed -e 's/.*<version>//' | sed -e 's/<\/version>.*//'`
-STORM_RELEASE=${STORM_RELEASE:-`grep -1 -A 0 -B 0 '<storm.default.version>' /vagrant/pom.xml | head -n 1 | awk '{print $1}' | sed -e 's/.*<storm.default.version>//' | sed -e 's/<\/storm.default.version>.*//'`}
-MESOS_RELEASE=${MESOS_RELEASE:-`grep -1 -A 0 -B 0 '<mesos.default.version>' /vagrant/pom.xml | head -n 1 | awk '{print $1}' | sed -e 's/.*<mesos.default.version>//' | sed -e 's/<\/mesos.default.version>.*//'`}
-CLASSIFIER=storm${STORM_RELEASE}-mesos${MESOS_RELEASE}
 
-cd /vagrant/_release/storm-mesos-${RELEASE}-${CLASSIFIER}
+cd /vagrant/_release/storm-mesos-${RELEASE}-*
 bin/storm jar /vagrant/vagrant/$*
